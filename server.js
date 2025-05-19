fb15703b0ece211ba2672a852f543f913f91811c
Manoel <mvclr1@gmail.com>
2025-05-19 12:11:42 -0300

    Arquivo base de servidor

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // Add this line to parse cookies
+
+// Routes
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
