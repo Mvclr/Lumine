13f0fb415aa760b3c0cb850238be9b92a7dfd581
Manoel <mvclr1@gmail.com>
2025-05-19 12:15:27 -0300

    JWTController

---
+++ controllers/jwtController.js
@@ -0,0 +1,20 @@
+const JWTAuth = require("../classes/jwtAuth.js");
+require("dotenv").config();
+const secretKey = process.env.SECRET || "MySecret";
+const jwtAuth = new JWTAuth(secretKey);
+
+function verifyJWT(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.redirect("/login");
+  }
+
+  try {
+    req.user = jwtAuth.verifyToken(token);
+    next();
+  } catch (err) {
+    return res.redirect("/login");
+  }
+}
+
+module.exports = { verifyJWT, jwtAuth };
