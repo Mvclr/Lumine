a7afe5794248f896dc6b7753e4b664e3570dccc6
Manoel <mvclr1@gmail.com>
2025-05-24 21:27:50 -0300

    Verificação de login
    Verifica se o usuário está logado

--- controllers/IsLoggedIn.js
+++ controllers/IsLoggedIn.js
@@ -1,6 +1,6 @@
 const { verifyJWT } = require("./jwtController.js");
 function isLoggedIn(req, res, next) {
-  const token = req.cookies?.token;
+  const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ loggedIn: false, message: "No token provided" });
   }
